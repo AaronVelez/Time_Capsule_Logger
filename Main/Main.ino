cfec2da06f38091c222a6a7165c62225d1300d03
Aarón I. Vélez Ramírez <aaron.velez@comunidad.unam.mx>
2021-11-10 23:09:04 -0600

    Visual Studio proyect created

---
+++ Main/Main.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Main.ino
+ Created:	11/10/2021 11:07:04 PM
+ Author:	aivel
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
