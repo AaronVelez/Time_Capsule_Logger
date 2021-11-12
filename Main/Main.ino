92d09e4f2af1a38ab03c6a80bc13f2f759a77fc7
Aarón I. Vélez Ramírez <aaron.velez@comunidad.unam.mx>
2021-11-12 16:21:28 -0600

    Main details

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,7 +1,21 @@
 /*
  Name:		Main.ino
  Created:	11/10/2021 11:07:04 PM
- Author:	aivel
+ Author:	Aarón I. Vélez Ramírez
+ 
+ Universidad Nacional Autónoma de México
+ 
+ Code for Time Capsule Logger ENES León
+ 
+ Details:
+ 
+ RTC adjust via Serial
+ ADC AD7193 used only for internal temperature data, it uses a modified library to use pin 11 instead of 10
+ Fan Speed control via DAC with N-MOSFET and feedback with sense resistor
+ Battery voltage monitor with resistor divider
+ No xbee comunication yet
+ No SDI-12 for soil water content meter yet
+ No missing SD card detect yet
 */
 
 // the setup function runs once when you press reset or power the board
