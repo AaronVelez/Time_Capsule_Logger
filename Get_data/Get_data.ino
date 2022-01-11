d53808fe6661fdbc5b6d3b2d3b4a9fc313126be9
Aarón I. Vélez Ramírez <aaron.velez@comunidad.unam.mx>
2022-01-10 22:25:33 -0600

    Fix "Get Data" code bugs

--- Get_data/Get_data.ino
+++ Get_data/Get_data.ino
@@ -66,8 +66,9 @@ float Temp = -1;        // Temperature
 
 
 ////// EEPROM variables
+uint32_t NextLog = 0;
 uint32_t EEPROM_Size = 0;
-uint32_t EEPROM_Addres = 0;
+uint32_t EEPROM_Address = 0;
 
 
 //////////////////////////////////////////////////////////////////////////
@@ -76,17 +77,38 @@ uint32_t EEPROM_Addres = 0;
 void setup() {
     // Start serial
     Serial.begin(115200);
+    delay(100);
+    Serial.println(F("Serial com started"));
 
 
     ////// Start I2C
     Wire.begin();
+    Serial.println(F("Wire com started"));
 
-
+    
     ///// Start EEPROM
-    myEEPROM.begin(0x54);
+    Serial.println("Starting EEPROM...");
+    Serial.print("Success: ");
+    Serial.println(myEEPROM.begin(0x54));
+    
+
+    ////// Read EEPROM header
+    myEEPROM.get(0, NextLog);
+    Serial.print(F("Next Log in address 0: "));
+    Serial.println(NextLog);
+
+    myEEPROM.get(4, EEPROM_Address);
+    Serial.print(F("EEPROM_Address in address 4: "));
+    Serial.println(EEPROM_Address);
+    
+    myEEPROM.get(8, local_t);
+    Serial.print(F("Local time at start: "));
+    Serial.println(local_t);
+
 
 
     // continue to loop 
+    Serial.println(F("Setup finished"));
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -95,18 +117,11 @@ void setup() {
 void loop() {
 
     ////// Check if Serial Monitor is ready to recieve data
-    if (Serial.available() == 'S') {
-        // Start EEPROM 1
-        myEEPROM.begin(0x50);
-        Serial.println("EEPROM 1 data:");
+    if (Serial.find('S')) {
+        Serial.println("EEPROM data:");
         // Get EEPROM 1 data and send via serial
         Get_Data();
-
-        // Start EEPROM 2
-        myEEPROM.begin(0x54);
-        Serial.println("EEPROM 2 data:");
-        // Get EEPROM 1 data and send via serial
-        Get_Data();        
+   
     }
 
 
@@ -115,30 +130,30 @@ void loop() {
 
 void Get_Data() {
     EEPROM_Size = myEEPROM.getMemorySize();
-    EEPROM_Addres = 8;
-    while (EEPROM_Addres < EEPROM_Size) {
+    EEPROM_Address = 8;
+    while (EEPROM_Address < EEPROM_Size) {
         // Print EEPROM address of current record
-        Serial.print(EEPROM_Addres);
+        Serial.print(EEPROM_Address);
         Serial.print("\t");
         //Get and print UNIX time of current record
-        myEEPROM.get(EEPROM_Addres, local_t);
+        myEEPROM.get(EEPROM_Address, local_t);
         Serial.print(local_t);
         Serial.print("\t");
         // Get and print Temperature of current record
-        EEPROM_Addres += 4;
-        myEEPROM.get(EEPROM_Addres, Temp);
+        EEPROM_Address += 4;
+        myEEPROM.get(EEPROM_Address, Temp);
         Temp = Temp / 10000;
         Serial.print(Temp);
         Serial.print("\t");
         // Get and print Battery voltage of current record
-        EEPROM_Addres += 2;
-        myEEPROM.get(EEPROM_Addres, BatVolt);
+        EEPROM_Address += 2;
+        myEEPROM.get(EEPROM_Address, BatVolt);
         BatVolt = BatVolt / 10000;
         Serial.print(BatVolt);
         Serial.print("\n");
         // Update EEPROM address for next recod            
-        EEPROM_Addres += 2;
+        EEPROM_Address += 2;
     }
     Serial.print("Done, bytes read: ");
-    Serial.print(EEPROM_Addres);
+    Serial.print(EEPROM_Address);
 }
